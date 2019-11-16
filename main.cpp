53f7c4ba39dbaad7b313c2003e514eaa680a4495
Simon@VirtualBoxMint <simondaniel321@gmail.com>
2019-11-16 17:45:08 +0100

    fixed bug, multiple entries of the same game arent longer stored as duplicates. console prints reduced

--- main.cpp
+++ main.cpp
@@ -18,10 +18,11 @@ int main(int argc, char* args[])
 	std::string name = args[3];
 	std::string title = args[4];
 	std::string emulator = args[5];
-	std::cout << numberOfEntries << std::endl;
-	std::cout << name << std::endl;
-	std::cout << title << std::endl;
-	std::cout << emulator << std::endl;
+	//std::cout << numberOfEntries << std::endl;
+	//std::cout << name << std::endl;
+	//std::cout << title << std::endl;
+	//std::cout << emulator << std::endl;
+	std::string newEntry = name + ";" + title + ";" + emulator + ";" + ";;;;;;;;;;;;;;";
 	
 	std::list<std::string*> inFileLines;
 	std::ifstream inStream(listPath);
@@ -38,7 +39,16 @@ int main(int argc, char* args[])
 				continue;
 			if(lines > numberOfEntries)
 				break;
-			inFileLines.push_back(lineptr);
+			if(lineptr->compare(newEntry) == 0)
+			{
+				lines--;
+				delete lineptr;
+			}
+			else
+			{
+				inFileLines.push_back(lineptr);
+			}
+			
 			lineptr = new std::string;
 		}
 		inStream.close();
@@ -49,25 +59,23 @@ int main(int argc, char* args[])
 		if(outStream.is_open())
 		{
 			outStream << firstLine << std::endl;
-			outStream << name << ";" << title << ";" << emulator << ";" << ";;;;;;;;;;;;;;" << std::endl;
+			outStream << newEntry << std::endl;
 			int i = 0;
-			std::cerr << "listlength" << inFileLines.size() << std::endl;
     		for (std::list<std::string*>::iterator it = inFileLines.begin(); it != inFileLines.end(); ++it)
     		{
-    			std::cerr << "writing: " << **it << std::endl;
+    			//std::cerr << "writing: " << **it << std::endl;
     			outStream << **it << std::endl;
     			delete *it;
     			
     			i++;
     		}
 			outStream.close();
+			std::cout << "[LastPlayedCreator] generated rom list" << std::endl;
 		}
 		else
 		{
 			std::cerr << "could not open out file: " << listPath << std::endl;
 		}
-		
-		std::cout << lines << " lines" << std::endl;
 	}
 	else
 	{
